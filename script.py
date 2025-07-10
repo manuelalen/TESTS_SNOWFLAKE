797b89a8dee117613fc042791f04bbac5d30d906
Manuel Alén Sánchez <97231769+manuelalen@users.noreply.github.com>
2025-07-10 21:06:03 +0200

    Update script.py

--- script.py
+++ script.py
@@ -4,7 +4,7 @@ import os
 conn = snowflake.connector.connect(
     user='manolitoalen',
     password='Manolitosnowflake_13',
-    account='nc47394.azure-westeurope',  # ← ¡ESTE es el valor correcto!
+    host='qvslpyc-nc47394.westeurope.azure.snowflakecomputing.com',  # ← ¡ESTE es el valor correcto!
     warehouse='COMPUTE_WH',
     database='DEV_LEARN_DWH',
     schema='TEST_SCHEMA',
