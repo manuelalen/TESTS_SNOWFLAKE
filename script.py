eb9ce4df40b5c6f92e5bdeb5a24a164501823b67
Manuel Alén Sánchez <97231769+manuelalen@users.noreply.github.com>
2025-07-10 21:16:11 +0200

    Update script.py

--- script.py
+++ script.py
@@ -4,17 +4,17 @@ import os
 conn = snowflake.connector.connect(
     user='manolitoalen',
     password='Manolitosnowflake_13',
-    account='nc47394',  # este valor puede ser obligatorio aunque se use host
-    host='qvslpyc-nc47394.westeurope.azure.snowflakecomputing.com',
+    account='nc47394.westeurope.azure',  # ❗️Este es tu account + región correcta
     warehouse='COMPUTE_WH',
     database='DEV_LEARN_DWH',
     schema='TEST_SCHEMA',
     role='SYSADMIN'
 )
 
-cs = conn.cursor()
-try:
-    cs.execute("SELECT CURRENT_VERSION()")
-    print(cs.fetchone())
-finally:
-    cs.close()
+with conn.cursor() as cur:
+    with open('TB_SNOWFLAKE_1_0_0.sql', 'r') as f:
+        sql_script = f.read()
+    cur.execute(sql_script)
+    print("✅ Script ejecutado correctamente.")
+
+conn.close()
