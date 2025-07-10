33435745be06bdbdbacd2138df0f09e0b9e40071
Manuel Alén Sánchez <97231769+manuelalen@users.noreply.github.com>
2025-07-10 20:59:10 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,30 +1,19 @@
-import os
 import snowflake.connector
-import glob
+import os
 
-# Conexión a Snowflake
 conn = snowflake.connector.connect(
-    user=os.environ["SNOWSQL_USER"],
-    password=os.environ["SNOWSQL_PWD"],
-    account=os.environ["SNOWSQL_ACCOUNT"],
-    warehouse="COMPUTE_WH",
-    database="DEV_LEARN_DWH",
-    schema="TEST_SCHEMA",
-    role="SYSADMIN"
+    user=os.environ['SNOWFLAKE_USER'],
+    password=os.environ['SNOWFLAKE_PASSWORD'],
+    account='nc47394.azure-westeurope',  # ← ¡ESTE es el valor correcto!
+    warehouse='COMPUTE_WH',
+    database='DEV_LEARN_DWH',
+    schema='TEST_SCHEMA',
+    role='SYSADMIN'
 )
-cur = conn.cursor()
 
+cs = conn.cursor()
 try:
-    sql_files = sorted(glob.glob("*.sql"))
-    for file in sql_files:
-        print(f"⏳ Ejecutando: {file}")
-        with open(file, "r") as f:
-            sql = f.read()
-            for stmt in sql.split(";"):
-                stmt = stmt.strip()
-                if stmt:
-                    print(f"🟢 Ejecutando sentencia:\n{stmt}")
-                    cur.execute(stmt)
+    cs.execute("SELECT CURRENT_VERSION()")
+    print(cs.fetchone())
 finally:
-    cur.close()
-    conn.close()
+    cs.close()
